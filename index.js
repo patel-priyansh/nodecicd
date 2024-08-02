8647dadee6d63a9e44bd9b3e6c62b67d51305dc1
patel-priyansh <145686618+patel-priyansh@users.noreply.github.com>
2024-08-02 04:11:01 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hello: "from develpoer." });
+  res.send({ hello: "from QA." });
 });
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 7000;
 app.listen(PORT, function() {
   console.log(`App listening on port ${PORT}`);  
 }); 
- 
