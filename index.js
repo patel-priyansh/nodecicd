ea72b4a39f32faed3f552629418f7a27c862bf03
patel-priyansh <145686618+patel-priyansh@users.noreply.github.com>
2024-08-02 12:20:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get("/", (req, res) => {
   res.send({ hello: "Agrisathi From Prod Server." }); 
 });
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, function() {
   console.log(`App listening on port ${PORT}`);  
 }); 
