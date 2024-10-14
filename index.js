684d6ba37b7681572375a7da121f44cb59dddffb
patel-priyansh <145686618+patel-priyansh@users.noreply.github.com>
2024-10-14 17:56:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hello: "from Production and team." });
+  res.send({ hello: "froom Production and team." });
 });
 
 const PORT = process.env.PORT || 4000;
